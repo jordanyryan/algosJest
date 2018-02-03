75bcfe9ab97a9beb24d4d5ed9c56fe7b35331ab1
Jordany Rosas <jordanyryan@gmail.com>
2018-02-03 00:05:27 -0500

    Added algo and test for factorial

---
+++ Algorithms/factorial/index.js
@@ -0,0 +1,22 @@
+// Given a number, n, return the factorial of that number
+
+// Examples -
+// factorial(5) => 120
+// 5 * 4 * 3 * 2 * 1 => 120
+
+// factorial(4) => 24
+// 4 * 3 * 2 * 1 => 24
+// Bonus - Use a memoize function to cache with recursion
+
+function factorial(n) {
+
+}
+
+function memoize(fn) {
+
+}
+
+// If you want to test for memoize function, 
+// go to test.js and uncomment the tests
+
+module.exports = {factorial, memoize}
---
+++ Algorithms/factorial/test.js
@@ -0,0 +1,36 @@
+const {factorial, memoize} = require('./index');
+
+test('factorial is a function', () => {
+  expect(typeof factorial).toEqual('function');
+});
+
+
+test('expects factorial of 5 is 120', () => {
+  expect(factorial(5)).toEqual(120);
+});
+
+test('expects factorial of 6 is 720', () => {
+  expect(factorial(6)).toEqual(720);
+})
+
+test('expect factorial of 10 is 3628800', () => {
+  expect(factorial(10)).toEqual(3628800);
+})
+
+test('expects factorial of 4 to NOT be 120', () => {
+  expect(factorial(4)).not.toEqual(120);
+});
+
+// test('memoize is a function', () => {
+//   expect(typeof memoize).toEqual('function');
+// });
+
+// test('you can memoize the factorial function', () => {
+//     expect(typeof memoize(factorial)).toEqual('function');
+// });
+
+// test('memoization of factorial of 5 is still 120', () => {
+//     factorial = memoize(factorial)
+//     expect(typeof memoize(factorial(5))).toEqual(120);
+// });
+
