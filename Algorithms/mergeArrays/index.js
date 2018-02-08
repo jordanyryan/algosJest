0c87163872ac0eb3d5af6b47ebabe5252910db81
Jordany Rosas <jordanyryan@gmail.com>
2018-02-07 23:32:05 -0500

    Added algo, working on test now

---
+++ Algorithms/mergeArrays/index.js
@@ -0,0 +1,15 @@
+// Given 2 sorted arrays, a and b,
+// can you merge these 2 arrays in
+// a way that the new array is also sorted?
+
+// Example - 
+// const a = [1,3,5,7,9,10];
+// const b = [1,2,3,5,7,8,11,12];
+
+// function mergeArrays(a,b) => [1,1,2,3,3,5,5,7,7,8,9,10,11,12];
+
+function mergeArrays(a,b) {
+}
+
+
+module.exports = mergeArrays;
---
+++ Algorithms/mergeArrays/test.js
@@ -0,0 +1,13 @@
+const mergeArrays = require('./index');
+
+// test("Tests to see that mergeArrays is a function", () => {
+//   expect(typeof mergeArrays).toEqual(true);
+// });
+
+// test("Tests to see that mergeArrays returns an array", () => {
+//   expect(Array.isArray(mergeArrays([1,3,4],[2,3,5]))).toEqual(true);
+// });
+
+// test("Expects mergeArrays to successfully merge and sort 2 sorted arrays", () => {
+//   expect(mergeArrays([1,3,4],[2,3,5])).toEqual([1,2,3,3,4,5]);
+// });
