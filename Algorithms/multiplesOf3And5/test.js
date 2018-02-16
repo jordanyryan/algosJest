d876263a0cffbafa97ac18fb3d449b78aab55473
Jordany Rosas <jordanyryan@gmail.com>
2018-02-15 22:47:14 -0500

    ADded some tests for multiplesOf3And5

---
+++ Algorithms/multiplesOf3And5/test.js
@@ -0,0 +1,26 @@
+const sumOfMultiples = require('./index');
+
+test("Tests to see that sumOfMultiples is a function", () => {
+  expect(typeof sumOfMultiples).toEqual('function');
+});
+
+test("Tests to see that sumOfMultiples returns a number", () => {
+  expect(typeof sumOfMultiples(10)).toEqual('number');
+});
+
+test("Expects sumOfMultiples to correctly return sum of multiples of 3 and 5", () => {
+  expect(sumOfMultiples(10)).toEqual(23);
+});
+
+test("Expects sumOfMultiples to correctly return sum of multiples of 3 and 5", () => {
+  expect(sumOfMultiples(15).toEqual(45);
+});
+
+test("Expects sumOfMultiples to correctly return sum of multiples of 3 and 5", () => {
+  expect(sumOfMultiples(20)).toEqual(78);
+});
+
+test("Expects sumOfMultiples to incorrectly return sum of multiples of 3 and 5", () => {
+  expect(sumOfMultiples(20)).not.toEqual(45);
+});
+
