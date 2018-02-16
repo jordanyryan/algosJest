6fa3ef5e69b23177737b19abe8ad9054355b9ba2
Jordany Rosas <jordanyryan@gmail.com>
2018-02-15 22:40:00 -0500

    Added algo for multiplesOf3And5

---
+++ Algorithms/multiplesOf3And5/index.js
@@ -0,0 +1,17 @@
+// Given a number, return the sum of all multiples
+// of 3 and 5 up to but not including
+// the number given
+
+// Example -
+// sumOfMultiples(10) => 23
+// 3 + 5 + 6 + 9 = 23
+
+// sumOfMultiples(20) =>  
+// 3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 = 78
+
+// multiples of 3 & 5 are only counted once
+
+function sumOfMultiples(number) {
+}
+
+module.exports = sumOfMultiples;
