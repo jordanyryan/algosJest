2846bf8140651c111dbc5ccf6283db1b58c2d6d4
Jordany Rosas <jordanyryan@gmail.com>
2018-02-21 23:45:10 -0500

    Added tests for queensPossibleMoves

--- Algorithms/queensPossibleMoves/test.js
+++ Algorithms/queensPossibleMoves/test.js
@@ -7,3 +7,19 @@ test("Tests to see that queensPossibleMovies is a function", () => {
 test("Tests to see that queensPossibleMovies returns a number", () => {
   expect(typeof queensPossibleMovies(5, [4,3], [[5,5], [4,2], [2,3]])).toEqual('number');
 });
+
+test("Tests to see that queensPossibleMovies returns the correct number", () => {
+  expect(queensPossibleMovies(5, [4,3], [[5,5], [4,2], [2,3]])).toEqual(10);
+});
+
+test("Tests to see that queensPossibleMovies returns the correct number", () => {
+  expect(queensPossibleMovies(5, [4,3], [[5,5],[5,2] [4,2], [2,3]])).toEqual(9);
+});
+
+test("Tests to see that queensPossibleMovies returns the correct number", () => {
+  expect(queensPossibleMovies(5, [4,3], [[5,5],[5,2] [4,2], [2,3], [3,2]])).toEqual(7);
+});
+
+test("Tests to see that queensPossibleMovies returns the correct number", () => {
+  expect(queensPossibleMovies(5, [4,3], [[5,5],[5,2] [4,2], [2,3], [2,5]])).toEqual(7);
+});
